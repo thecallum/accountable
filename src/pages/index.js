77de8624b6208d03e1a431b685c0ea7620437828
Callum Macpherson <callummac@protonmail.com>
2019-11-26 21:57:50 +0000

    Added context to home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,17 @@
 import React from "react"
 import Layout from "../components/layout"
 import { Link } from "gatsby"
+import { Security } from "../services/security"
 
 export default () => {
   return (
-    <Layout>
-      <h1>Home page</h1>
-      <p>About the app</p>
+    <Security>
+      <Layout>
+        <h1>Home page</h1>
+        <p>About the app</p>
 
-      <Link to="/app/login/">Login</Link>
-    </Layout>
+        <Link to="/app/login/">Login</Link>
+      </Layout>
+    </Security>
   )
 }
