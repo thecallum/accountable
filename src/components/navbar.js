d5f8f0bb1d225ed1cbca37e493b5902aca554672
Callum Macpherson <callummac@protonmail.com>
2019-11-26 19:39:24 +0000

    Styled navbar

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -1,18 +1,42 @@
 import React from "react"
 import { Link } from "gatsby"
-
 import { Consumer } from "../services/security"
+import Styled from "styled-components"
+
+const Navbar = Styled.nav`
+  background-color: hsl(0, 50%, 50%);
+  padding: 15px;
+
+  display: flex;
+  justify-content: space-between;
+
+  .title {
+    font-weight: 100;
+    font-size: 28px;
+    color: #fff;
+    font-family: sans-serif;
+  }
+
+  ul {
+    margin: 0;
+    padding: 0;
+    display: flex;
+    margin-right: -10px;
+    flex-wrap: wrap;
+    align-items: center;
+  }
+
+  li {
+    display: block;
+    margin-right: 10px;
+  }
+`
 
 export default () => (
   <Consumer>
     {context => (
-      <nav
-        style={{
-          backgroundColor: "red",
-          padding: "15px",
-        }}
-      >
-        <span>Navbar</span>
+      <Navbar>
+        <div className="title">Navbar</div>
 
         <ul>
           <li>
@@ -32,7 +56,7 @@ export default () => (
             </li>
           )}
         </ul>
-      </nav>
+      </Navbar>
     )}
   </Consumer>
 )
